e92b52560480f377127c067042e0999c276bea6d
Tsengee <tsngl9999@gmail.com>
2020-07-31 11:28:57 +0800

    gitpage ashiglahiin tuld docs folderiig uusgelee

---
+++ src/js/model/Search.js
@@ -0,0 +1,15 @@
+require("@babel/polyfill");
+import axios from "axios";
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+  async doSearch() {
+    let result = await axios(
+      "https://forkify-api.herokuapp.com/api/search?q=" + this.query
+    );
+    this.result = result.data.recipes;
+    return this.result;
+  }
+}
