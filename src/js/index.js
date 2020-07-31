e19cf446ecfb1ca0a026dd869a006a207a39ea50
Tsengee <tsngl9999@gmail.com>
2020-07-31 14:14:17 +0800

    state bolon search controller uusgelee

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,23 @@
 require("@babel/polyfill");
 import Search from "./model/Search";
 
-let search = new Search("pasta");
+const state = {};
 
-search.doSearch().then((r) => console.log(r));
+const controlSearch = async () => {
+  // 1) Вэбээс хайлтын түлхүүр үгийг гаргаж авна
+  const query = "pizza";
+  if (query) {
+    // 2) Шинээр хайлтын обьектыг үүсгэж өгнө
+    state.search = new Search(query);
+    // 3) Хайлт хийхэд зориулж UI г бэлтгэнэ.
+    // 4)  Хайтыг гүйцэтгэнэ
+    await state.search.doSearch();
+    //5) Хайлтын үр дүнг дэлгэцэнд үзүүлнэ.
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector(".search").addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
+});
